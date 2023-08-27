898faeb4526d90b8796a59b2d17fb0b66ccaa63c
louisfiy <82554884+louisfiy@users.noreply.github.com>
2023-08-27 22:50:09 +0800

    Site updated: 2023-08-27 22:50

--- _astro/p20150516.ef987feb.js
+++
@@ -1,9 +0,0 @@
-import{c as s}from"./index.c97107e6.js";import{s as c}from"./index.5e636078.js";import{F as a}from"./Favorite.218f086a.js";import"./dayjs.min.63f98895.js";import"./_plugin-vue_export-helper.c27b6911.js";import"./runtime-core.esm-bundler.45595617.js";const r={};function i(e){return e.replaceAll(/__ASTRO_IMAGE_="([^"]+)"/gm,(t,n)=>c({src:r[n].src,...r[n].attributes}))}const o=i(`<p>听朋友说，大四学年很闲很悠哉，可是我看他从去年九月忙到现在，貌似就没有停过。朋友去年十月上完课程，中旬选罢毕设课题，十一月就去了实训，十二月回来又拖了几个星期弄课设，过完元旦才开始搞代码，过年聚会完该干啥干啥，三月回校继续敲，论文从清明写到五一，修修改改马上又到了毕业季，拍照前脚刚走，答辩后脚就来，完了以后朋友一如既往忧郁地对我说，你以为这样就解脱了吗？</p>
-<p>我知道，那时候对他来说很多事情都还充满着未知数：论文还没定稿、答辩还没启动、典礼还没举行、工作还没着落……对此我没有反驳。我说，你辛苦这四年，不就为了这一天么？他说，道理我都懂，可生活状态的改变对我而言无疑是道坎儿。我说，你不去试试你又怎么知道。他说，没说我不试试……然后巴拉巴拉讲一堆。至此我不难得出“朋友是个处女座”的结论，反正我觉得没什么好担心的，也许一个月总有那么几天呢。</p>
-<p>于是我说：正所谓天将降大任于斯人也，必先苦其心志，劳其筋骨，饿……对了，你饿不饿，我下碗面给你吃？</p>`),m={title:"记一次毕业",date:"2015-05-16T00:17:21.000Z",categories:["随笔"],tags:["生活"]},u="D:/Shin/astro-site/src/content/doc/p20150516.md",p=void 0;function b(){return`\r
-听朋友说，大四学年很闲很悠哉，可是我看他从去年九月忙到现在，貌似就没有停过。朋友去年十月上完课程，中旬选罢毕设课题，十一月就去了实训，十二月回来又拖了几个星期弄课设，过完元旦才开始搞代码，过年聚会完该干啥干啥，三月回校继续敲，论文从清明写到五一，修修改改马上又到了毕业季，拍照前脚刚走，答辩后脚就来，完了以后朋友一如既往忧郁地对我说，你以为这样就解脱了吗？\r
-\r
-我知道，那时候对他来说很多事情都还充满着未知数：论文还没定稿、答辩还没启动、典礼还没举行、工作还没着落……对此我没有反驳。我说，你辛苦这四年，不就为了这一天么？他说，道理我都懂，可生活状态的改变对我而言无疑是道坎儿。我说，你不去试试你又怎么知道。他说，没说我不试试……然后巴拉巴拉讲一堆。至此我不难得出“朋友是个处女座”的结论，反正我觉得没什么好担心的，也许一个月总有那么几天呢。\r
-\r
-于是我说：正所谓天将降大任于斯人也，必先苦其心志，劳其筋骨，饿……对了，你饿不饿，我下碗面给你吃？\r
-`}function y(){return o}function C(){return[]}async function f(){const{layout:e,...t}=m;return t.file=u,t.url=p,s(a,{"set:html":o})}f[Symbol.for("astro.needsHeadRendering")]=!0;export{f as Content,y as compiledContent,f as default,u as file,m as frontmatter,C as getHeadings,r as images,b as rawContent,p as url};
